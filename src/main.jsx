0156b82a95f20e316b716bc2675aeb75adb0d4dd
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2023-02-18 10:20:01 +0700

    revamp height section + add Swiper

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Index from './pages/Index'
 
+import { register } from 'swiper/element/bundle'
+
+register()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Index/>
--- src/pages/partial-section/WhatWeDo.jsx
+++ src/pages/partial-section/WhatWeDo.jsx
@@ -1,14 +1,34 @@
+import React from 'react';
 export default function WhatWeDo() {
+
   return(
     <section className="section-vh-75 section-what-we-do">
-      <div className="container-fluid section-what-we-do-inner section-inner">
+      <div className="container-fluid section-what-we-do-inner section-inner text-center">
         <div className="row">
           <div className="col-12">
             <h4 className="section-subtitle section-subtitle-with-bg">What We Do</h4>
             <h1>WE SPECIALIZE IN ONE THING, AND WE DO IT PERFECTLY: EDITING YOUR WEDDING FILM</h1>
             <p>We bring your couple's tales to life, tailored to your vision. Our streamlined approach allows you to prioritize your work, family, or other pursuits. Streamline your workload. Produce high-quality films in less time. Experience peace of mind.</p>
             <div className="section-what-we-do-galleries">
-
+              {/* <div id="lightgallery">
+                <a href="/src/assets/images/1066_websize.jpg" data-lg-size="1024-800">
+                  <img src="/src/assets/images/1066_websize.jpg" alt="placeholder" />
+                </a>
+                <a href="/src/assets/images/1066_websize.jpg" data-lg-size="1024-800">
+                  <img src="/src/assets/images/1066_websize.jpg" alt="placeholder" />
+                </a>
+              </div> */}
+              <swiper-container>
+                <swiper-slide>
+                  <img src="/src/assets/images/1066_websize.jpg" alt="placeholder" />
+                </swiper-slide>
+                <swiper-slide>
+                  <img src="/src/assets/images/1066_websize.jpg" alt="placeholder" />
+                </swiper-slide>
+                <swiper-slide>
+                  <img src="/src/assets/images/1066_websize.jpg" alt="placeholder" />
+                </swiper-slide>
+              </swiper-container>
             </div>
             <button className="btn btn-primary">View Our Extended Work</button>
           </div>
