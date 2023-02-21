d5eefea4616e6883b9b589fb54fce920b5ff8593
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2023-02-21 18:15:49 +0700

    add env production

--- src/helper/api.js
+++ src/helper/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
-import process from "process";
 
 export const getFaqs = () => {
-  return axios.get(`${import.meta.env.VITE_API_URL}/faqs`);
+  return axios.get(
+    `${import.meta.env.VITE_API_URL}/faq`,
+    {
+      Accept: "application/json",
+      // allow cors 
+      mode: "cors",
+    },
+  )
 };
