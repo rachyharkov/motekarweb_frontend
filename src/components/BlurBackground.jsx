b3da16096e3c27bab7fbd9922f40e7043d3a0c50
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2023-03-01 11:53:24 +0700

    init

--- src/components/BlurBackground.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useEffect } from "react";
-export default function BlurBackground() {
-
-  return (
-    <div className="blur-background animate__animated animate__fadeIn"></div>
-  )
-}
--- src/helper/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getFaqs = () => {
-  return axios.get(
-    `${import.meta.env.VITE_API_URL}/faq`)
-};
